7332d46d3c124b66107caf5041af01e173c40033
Jeremy Bícha <jbicha@ubuntu.com>
2023-05-19 16:18:13 -0400

    display: Set compositor selection earlier on XWayland
    Gbp-Pq: display-Set-compositor-selection-earlier-on-XWayland.patch.

--- src/tests/test-client.c
+++ src/tests/test-client.c
@@ -1003,6 +1003,10 @@ main(int argc, char **argv)
   gtk_init (NULL, NULL);
 
   screen = gdk_screen_get_default ();
+
+  if (!wayland)
+    g_assert_true (gdk_screen_is_composited (screen));
+
   provider = gtk_css_provider_new ();
   static const char *no_decoration_css =
     "decoration {"

84eea68df4dc5c70348a921f03ef5a3665f1ea04
Daniel van Vugt <daniel.van.vugt@canonical.com>
2023-07-12 07:49:16 -0400

    clutter/frame-clock: Avoid rapidly toggling dynamic max render time
    Gbp-Pq: clutter-frame-clock-Avoid-rapidly-toggling-dynamic-max-re.patch.

--- clutter/clutter/clutter-frame-clock.c
+++ clutter/clutter/clutter-frame-clock.c
@@ -109,6 +109,7 @@ struct _ClutterFrameClock
   EstimateQueue swap_to_flip_us;
   /* If we got new measurements last frame. */
   gboolean got_measurements_last_frame;
+  gboolean ever_got_measurements;
 
   gboolean pending_reschedule;
   gboolean pending_reschedule_now;
@@ -312,6 +313,7 @@ clutter_frame_clock_notify_presented (ClutterFrameClock *frame_clock,
                                 swap_to_flip_us);
 
       frame_clock->got_measurements_last_frame = TRUE;
+      frame_clock->ever_got_measurements = TRUE;
     }
   else
     {
@@ -386,7 +388,7 @@ clutter_frame_clock_compute_max_render_time_us (ClutterFrameClock *frame_clock)
 
   refresh_interval_us = frame_clock->refresh_interval_us;
 
-  if (!frame_clock->got_measurements_last_frame ||
+  if (!frame_clock->ever_got_measurements ||
       G_UNLIKELY (clutter_paint_debug_flags &
                   CLUTTER_DEBUG_DISABLE_DYNAMIC_MAX_RENDER_TIME))
     return refresh_interval_us * SYNC_DELAY_FALLBACK_FRACTION;

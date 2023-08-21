b27862d24a0b7a0bd115d497d66681eca4cc80c7
Jeremy Bícha <jbicha@ubuntu.com>
2023-08-21 11:19:08 -0400

    Support Dynamic triple/double buffering
    Gbp-Pq: debian/Support-Dynamic-triple-double-buffering.patch.

--- src/backends/native/meta-kms-crtc.h
+++ src/backends/native/meta-kms-crtc.h
@@ -24,6 +24,7 @@
 #include <xf86drmMode.h>
 
 #include "backends/native/meta-kms-types.h"
+#include "backends/native/meta-swap-chain.h"
 #include "backends/meta-backend-types.h"
 #include "core/util-private.h"
 #include "meta/boxes.h"
@@ -62,3 +63,5 @@ int meta_kms_crtc_get_idx (MetaKmsCrtc *crtc);
 
 META_EXPORT_TEST
 gboolean meta_kms_crtc_is_active (MetaKmsCrtc *crtc);
+
+MetaSwapChain * meta_kms_crtc_get_swap_chain (MetaKmsCrtc *crtc);

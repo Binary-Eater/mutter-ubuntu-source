35cbdcd1c15eb805e62184e704e37c6ea3c8cdb4
Daniel van Vugt <daniel.van.vugt@canonical.com>
2023-09-08 10:40:24 -0400

    window: Add ability to override the edge constraints
    Gbp-Pq: ubuntu/window-Add-ability-to-override-the-edge-constraints.patch.

--- src/core/window-private.h
+++ src/core/window-private.h
@@ -230,6 +230,13 @@ struct _MetaWindow
     MetaEdgeConstraint left;
   } edge_constraints;
 
+  struct {
+    MetaEdgeConstraint top;
+    MetaEdgeConstraint right;
+    MetaEdgeConstraint bottom;
+    MetaEdgeConstraint left;
+  } overridden_constraints;
+
   double tile_hfraction;
 
   uint64_t preferred_output_winsys_id;

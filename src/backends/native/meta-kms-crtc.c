0fab5a4994f2c3273136ff28909c3472db791ac1
Jeremy Bícha <jbicha@ubuntu.com>
2024-07-12 18:26:34 -0400

    kms/crtc: Increase default deadline evasion to 1000 microseconds
    Gbp-Pq: workarounds/kms-crtc-Increase-default-deadline-evasion-to-1000-micros.patch.

--- src/backends/native/meta-kms-crtc.c
+++ src/backends/native/meta-kms-crtc.c
@@ -28,7 +28,7 @@
 #include "backends/native/meta-kms-update-private.h"
 #include "backends/native/meta-kms-utils.h"
 
-#define DEADLINE_EVASION_US 800
+#define DEADLINE_EVASION_US 1000
 #define DEADLINE_EVASION_WITH_KMS_TOPIC_US 1000
 
 #define MINIMUM_REFRESH_RATE 30.f

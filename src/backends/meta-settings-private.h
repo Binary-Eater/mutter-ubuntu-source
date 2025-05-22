6de141af83da0db5612ac221892e3a2c807559f0
Nathan Pratta Teodosio <nteodosio@ubuntu.com>
2025-05-22 10:55:41 +0200

    x11-Add-support-for-fractional-scaling-using-Randr
    Gbp-Pq: ubuntu/x11-Add-support-for-fractional-scaling-using-Randr.patch.

--- src/backends/meta-settings-private.h
+++ src/backends/meta-settings-private.h
@@ -34,6 +34,7 @@ typedef enum _MetaExperimentalFeature
   META_EXPERIMENTAL_FEATURE_AUTOCLOSE_XWAYLAND  = (1 << 2),
   META_EXPERIMENTAL_FEATURE_VARIABLE_REFRESH_RATE = (1 << 3),
   META_EXPERIMENTAL_FEATURE_XWAYLAND_NATIVE_SCALING  = (1 << 4),
+  META_EXPERIMENTAL_FEATURE_X11_RANDR_FRACTIONAL_SCALING = (1 << 5),
 } MetaExperimentalFeature;
 
 typedef enum _MetaXwaylandExtension
@@ -41,6 +42,13 @@ typedef enum _MetaXwaylandExtension
   META_XWAYLAND_EXTENSION_SECURITY = (1 << 0),
   META_XWAYLAND_EXTENSION_XTEST = (1 << 1),
 } MetaXwaylandExtension;
+
+typedef enum _MetaX11ScaleMode
+{
+  META_X11_SCALE_MODE_NONE = 0,
+  META_X11_SCALE_MODE_UP = 1,
+  META_X11_SCALE_MODE_UI_DOWN = 2,
+} MetaX11ScaleMode;
 
 #define META_TYPE_SETTINGS (meta_settings_get_type ())
 G_DECLARE_FINAL_TYPE (MetaSettings, meta_settings,
@@ -101,3 +109,8 @@ void meta_settings_set_output_luminance (MetaSettings          *settings,
 void meta_settings_reset_output_luminance (MetaSettings          *settings,
                                            const MetaMonitorSpec *monitor_spec,
                                            MetaColorMode          color_mode);
+
+MetaX11ScaleMode meta_settings_get_x11_scale_mode (MetaSettings *settings);
+
+void meta_settings_enable_x11_fractional_scaling (MetaSettings *settings,
+                                                  gboolean      enabled);
